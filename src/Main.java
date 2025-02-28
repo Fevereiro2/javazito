34f98421e37b58411424328de018e960005a61c1
Fevereiro2 <a2124a15@estel.edu.pt>
2025-02-28 19:09:32 +0000

    make code and comment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
+        Scanner read = new Scanner(System.in); // bibioteca para pedir dados a utilizadores
+        int x,y; //declarar variaveis
+        System.out.println("Insira dois numeros"); //dar print
+        x = read.nextInt(); //armazenar
+        y = read.nextInt(); //armazenar
+        if (x > y) //caso de decisão
+            System.out.println(x + " é maior que " + y); //dar print
+        else //se não
+            System.out.println(x + " é igual ou menor que " + y); //dar print
     }
 }
